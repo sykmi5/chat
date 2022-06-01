83daaa018688e2b9f54a7eb36b4184d990c20e5a
sykking <sykking@outlook.com>
2022-06-01 22:04:14 +0800

    hot_fix commit

--- src/main/java/com/sykking/controller/UserController.java
+++ src/main/java/com/sykking/controller/UserController.java
@@ -21,6 +21,8 @@ public class UserController {
     @GetMapping("/register")
     public void register(User user, HttpServletResponse response) throws IOException {
         System.out.println("2");
+
+        System.out.println("hotfix test");
         if (user != null){
             userService.addUser(user);
             response.sendRedirect("/chat/login.html");
