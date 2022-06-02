9c3d17f39469e59c3774828a9990a4e7710bd7dd
sykmi5 <106692768+sykmi5@users.noreply.github.com>
2022-06-02 16:27:00 +0800

    Update UserController.java
    11

--- src/main/java/com/sykking/controller/UserController.java
+++ src/main/java/com/sykking/controller/UserController.java
@@ -20,8 +20,6 @@ public class UserController {
 
     @GetMapping("/register")
     public void register(User user, HttpServletResponse response) throws IOException {
-        System.out.println("2");
-        System.out.println("hotfix test");
         if (user != null){
             userService.addUser(user);
             response.sendRedirect("/chat/login.html");
